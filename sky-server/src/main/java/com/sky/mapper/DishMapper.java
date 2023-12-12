e2bc95227c9a149821a372e79a28d11a78ae0211
jxt <2765129080@qq.com>
2023-12-12 11:56:59 +0800

    添加菜品代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,6 +1,10 @@
 package com.sky.mapper;
 
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -9,6 +13,10 @@ public interface DishMapper {
 
     @Select("select count(*) from sky_take_out.dish where category_id = #{id}")
     Integer getByCategoryId(Integer id);
+
+
+    @AutoFill(OperationType.INSERT)
+    long insert(Dish dish);
 }
 
 
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,45 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+    @Autowired
+    DishMapper dishMapper;
+    @Autowired
+    DishFlavorMapper dishFlavorMapper;
+
+    @Override
+    @Transactional  // 开启事务
+    public void save(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+
+        dishMapper.insert(dish);
+
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+
+        //判断一下是否添加了口味的选择, 如果没有添加就不需要在表中插入了
+        if (flavors != null && flavors.size() > 0) {
+        flavors.stream().forEach(
+                flavor -> flavor.setDishId(dish.getId())
+        );
+        dishFlavorMapper.insert(flavors);
+        }
+
+
+
+
+    }
+}
